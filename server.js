b93ddb34569d19b527d61495046f76504d575680
Hossam Khamis <hossams.k@gmail.com>
2021-02-20 23:08:53 +0200

    Add mongoose and connect to DB.

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
 const {CAMPAIGNS_ROUTE} = require('./constants');
+const connectDB = require('./config/db');
 // Route files
 const campaigns = require('./routes/campaigns');
 
 // Load env variables
 dotenv.config({path: './config/config.env'});
+
+// Connect to database
+connectDB();
 
 const app = express();
 
@@ -21,4 +24,11 @@ app.use(CAMPAIGNS_ROUTE, campaigns);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`));
+
+// Handle unhandled promise exceptions
+process.on('unhandledRejection', (error, promise) => {
+  console.log(`Error: ${error.message}`);
+  // Close server and exist process
+  server.close(() => process.exit(1));
+});
