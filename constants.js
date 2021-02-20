aa61dedb9ae7e5e8a0a10cedda8a2ef24897c84a
Hossam Khamis <hossams.k@gmail.com>
2021-02-20 18:00:33 +0200

    Add campaigns routes.

---
+++ constants.js
@@ -0,0 +1,6 @@
+const BASE_ROUTE = '/api/v1';
+
+module.exports = Object.freeze({
+  BASE_ROUTE: BASE_ROUTE,
+  CAMPAIGNS_ROUTE: `${BASE_ROUTE}/campaigns`,
+});
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const {CAMPAIGNS_ROUTE} = require('./constants');
+// Route files
+const campaigns = require('./routes/campaigns');
 
+// Load env variables
 dotenv.config({path: './config/config.env'});
 
 const app = express();
+
+// Mount routes
+app.use(CAMPAIGNS_ROUTE, campaigns);
 
 const PORT = process.env.PORT || 5000;
 
