f5c6d52de77c0bce4f22f43d0cd928230070fac0
Abdelrahman Nabil <eng.nabil2014@gmail.com>
2021-02-26 18:04:13 +0100

    Add login call to controller with jwt token With token returned on registeration and on successfull login response.

--- controllers/auth.js
+++ controllers/auth.js
@@ -28,7 +28,47 @@ exports.register = asyncHandler(async (req, res, next) => {
     gender,
     password,
   });
+
+  // Ceate token
+  const token = user.getSignedJwtToken();
+
   res.status(200).json({
     success: true,
+    token,
+  });
+});
+
+// @desc    Login user
+// @route   POST /api/v1/auth/login
+// @access  Public
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Validate email and password
+  if (!email || !password) {
+    return next(new ErrorResponse('Please provide an email and password', 400));
+  }
+
+  // Check user
+  const user = await User.findOne({ email }).select('+password');
+
+  // User not found
+  if (!user) {
+    return next(new ErrorResponse('Invalid credintials', 401));
+  }
+
+  // Check if password match
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse('Invalid credintials', 401));
+  }
+
+  // Ceate token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({
+    success: true,
+    token,
   });
 });
